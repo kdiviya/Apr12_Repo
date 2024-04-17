a7c8c3cbbd93732a66474960b67baa34830189f5
System Administrator <root@Srinivasans-MacBook-Air.local>
2024-04-17 16:53:28 -0500

    add test1

--- Test1.py
+++ Test1.py
@@ -1,3 +1,5 @@
+import time
+while True:
 num1 = 1.5
 num2 = 6.3
 
@@ -6,3 +8,4 @@ sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+time.sleep(5)
