e54854dd8e3d61e496b37402f26a0f3edbda5635
kdiviya <165975806+kdiviya@users.noreply.github.com>
2024-05-03 11:48:41 -0500

    Update Test2.py

--- Test1.py
+++ Test1.py
@@ -1,13 +1,4 @@
-import time
+# This program prints Hello, world!
 
-while True:
-    num1 = 1.5
-    num2 = 6.3
+print('Hello, world!')
 
-    # Add two numbers
-    sum = num1 + num2
-
-    # Display the sum
-    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
-    time.sleep(5)
